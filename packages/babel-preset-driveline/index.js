5945c37bd76d5924e6b46cb11c1ff3550a51126a
Markus J. Wetzel <markuswetzel@gmx.net>
2025-01-23 21:40:44 +0100

    Add expo option to babel preset

--- packages/babel-preset-driveline/index.js
+++ packages/babel-preset-driveline/index.js
@@ -1,29 +1,41 @@
 module.exports = (api, opts) => {
-  const { typescript = true, root = './app' } = opts;
+  const { typescript = true, flow = true, expo = false, root = './app' } = opts;
+
+  const commonPlugins = [
+    [
+      require.resolve('@loadable/babel-plugin'),
+      {
+        signatures: [{ name: 'default', from: 'driveline/lazy' }],
+      },
+    ],
+    [
+      require.resolve('babel-plugin-intlized-components'),
+      { ignoreImport: true, autoResolveKey: root },
+    ],
+  ];
+
+  if (expo) {
+    return {
+      plugins: commonPlugins,
+    };
+  }
 
   return {
     presets: [
       require.resolve('@babel/preset-env'),
       require.resolve('@babel/preset-react'),
       typescript && require.resolve('@babel/preset-typescript'),
+      flow && require.resolve('@babel/preset-flow'),
     ].filter(Boolean),
     plugins: [
-      [
-        require.resolve('@loadable/babel-plugin'),
-        {
-          signatures: [{ name: 'default', from: 'driveline/lazy' }],
-        },
-      ],
-      [
-        require.resolve('babel-plugin-intlized-components'),
-        { ignoreImport: true, autoResolveKey: root },
-      ],
+      ...commonPlugins,
       [
         require.resolve('babel-plugin-module-resolver'),
         {
           root,
         },
       ],
+      require.resolve('babel-plugin-react-native-web'),
     ],
   };
 };
